822d5473f1e1ca19ca5e60aa412962e81decc40a
Marco Dave <marcodave03@gmail.com>
2025-03-06 18:23:24 +0700

    .

--- models/UserAsset.js
+++ models/UserAsset.js
@@ -1,3 +1,21 @@
 import {DataType} from "sequelize";
 import db from "../config/Database";
 
+
+const UserAsset = db.define("UserAsset", {
+    id: {
+        type: DataType.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    assetname: {
+        type: DataType.STRING,
+        unique: true,
+    },
+    assetprice: {
+        type: DataType.INTEGER,
+        allowNull: false
+    },
+});
+
+export default User;
